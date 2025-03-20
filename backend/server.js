ed2fed1666308d2ab6f9ca03c64d0f2dcac469bb
prateek959 <prateekkush8081@gmail.com>
2025-03-20 23:37:24 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['http://127.0.0.1:5500', 'https://car-booking-app-123.netlify.app'],
     credentials: true
   }));
 
